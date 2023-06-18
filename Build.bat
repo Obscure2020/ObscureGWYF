c640c64d5a9e0fdfb0c051dc3a45ae848a5a2f00
Joey Sodergren <jas.elgoog@gmail.com>
2023-06-18 01:24:23 -0400

    Adding SVG Buttons

--- Build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-javac *.java && cls && java WebBuilder && goto Finish
-echo.
-pause
-:Finish
--- WebBuilder.java
+++ WebBuilder.java
@@ -3,12 +3,12 @@ import java.util.Arrays;
 
 class WebBuilder{
     public static void main(String[] args) throws Exception{
-        //URI test = new URI(null, "Candy Land", null);
+        //URI test = new URI(null, file.toString(), null);
         //System.out.println(test.toASCIIString());
-        File rootDir = new File(".");
-        File[] contents = Arrays.stream(rootDir.listFiles()).filter(f -> f.isDirectory()).toList().toArray(new File[0]);
-        for(File f : contents){
-            System.out.println(f.toString());
+        File folder = new File("./Workshop/Graveyard craving");
+        File[] images = Arrays.stream(folder.listFiles()).filter(f -> f.toString().toLowerCase().endsWith(".png")).toList().toArray(new File[0]);
+        for(File f : images){
+            System.out.println(f.getName().split("[.]")[0]);
         }
     }
 }
