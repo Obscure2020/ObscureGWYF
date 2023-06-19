d9faba11eb979a6e4d9125f436aa960a4a7992e1
Joey Sodergren <jas.elgoog@gmail.com>
2023-06-19 15:31:03 -0400

    HTML generating

--- WebBuilder.java
+++ WebBuilder.java
@@ -1,40 +1,62 @@
 import java.io.File;
-import java.util.ArrayList;
+import java.util.*;
 
 class WebBuilder{
+    private static void itemizeDirectories(File parent, ArrayList<WebPage> pages, ArrayList<WebPage> firsts) throws Exception{
+        File[] dirs = Arrays.stream(parent.listFiles()).filter(f -> f.isDirectory()).toList().toArray(new File[0]);
+        for(File folder : dirs){
+            ArrayDeque<WebPage> result = new ArrayDeque<>(18);
+            for(int hole=1; hole<=18; hole++){
+                File output = new File(folder, "Hole " + hole + ".html");
+                File guess = new File(folder, "Hole " + hole + ".png");
+                if(guess.exists()){
+                    File[] images = {guess};
+                    WebPage newPage = new WebPage(output, images);
+                    WebPage tail = result.peekLast();
+                    if(tail != null){
+                        tail.setNext(output);
+                        newPage.setPrevious(tail.getTarget());
+                    }
+                    result.addLast(newPage);
+                    continue;
+                }
+                guess = new File(folder, "Hole " + hole + "-1.png");
+                if(guess.exists()){
+                    File second = new File(folder, "Hole " + hole + "-2.png");
+                    if(!second.exists()){
+                        StringBuilder sb = new StringBuilder("Currently itemizing \"");
+                        sb.append(folder.getCanonicalPath());
+                        sb.append("\".\n\"");
+                        sb.append(guess.getName());
+                        sb.append("\" exists, but \"");
+                        sb.append(second.getName());
+                        sb.append("\" does not. File naming error.");
+                        throw new Exception(sb.toString());
+                    }
+                    File[] images = {guess, second};
+                    WebPage newPage = new WebPage(output, images);
+                    WebPage tail = result.peekLast();
+                    if(tail != null){
+                        tail.setNext(output);
+                        newPage.setPrevious(tail.getTarget());
+                    }
+                    result.addLast(newPage);
+                }
+            }
+            firsts.add(result.peekFirst());
+            for(WebPage wp : result) pages.add(wp);
+        }
+        System.gc();
+    }
     public static void main(String[] args) throws Exception{
         // URI root = new File(".").toURI();
         // System.out.println(root.toASCIIString());
         // File beans = new File("./Workshop/Graveyard craving/Hole 1.png");
         // System.out.println(root.relativize(beans.toURI()).toASCIIString());
         ArrayList<WebPage> pages = new ArrayList<>();
-        File folder = new File("./Workshop/Graveyard craving");
-        for(int hole=1; hole<=18; hole++){
-            File output = new File(folder, "Hole " + hole + ".html");
-            File guess = new File(folder, "Hole " + hole + ".png");
-            if(guess.exists()){
-                File[] images = {guess};
-                WebPage newPage = new WebPage(output, images);
-                pages.add(newPage);
-            }
-            guess = new File(folder, "Hole " + hole + "-1.png");
-            if(guess.exists()){
-                File second = new File(folder, "Hole " + hole + "-2.png");
-                if(!second.exists()){
-                    StringBuilder sb = new StringBuilder("\"");
-                    sb.append(guess.getName());
-                    sb.append("\" exists, but \"");
-                    sb.append(second.getName());
-                    sb.append("\" does not. File naming error.");
-                    throw new Exception(sb.toString());
-                }
-                File[] images = {guess, second};
-                WebPage newPage = new WebPage(output, images);
-                pages.add(newPage);
-            }
-        }
-        for(WebPage wp : pages){
-            wp.generate(null, null);
-        }
+        ArrayList<WebPage> firsts = new ArrayList<>();
+        itemizeDirectories(new File("./Workshop"), pages, firsts);
+        itemizeDirectories(new File("./Official"), pages, firsts);
+        for(WebPage wp : pages) wp.generate();
     }
 }
--- WebPage.java
+++ WebPage.java
@@ -1,19 +1,95 @@
-import java.io.File;
-import java.util.Arrays;
+import java.io.*;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
 
 public class WebPage {
     private File target;
     private File[] images;
     private static final String tab = "    ";
+    private File previous;
+    private File next;
 
     public WebPage(File newTarget, File[] newImages){
         target = newTarget;
         images = newImages;
+        previous = null;
+        next = null;
     }
 
-    public void generate(File previous, File next){
+    public void setPrevious(File newPrev){
+        previous = newPrev;
+    }
+
+    public void setNext(File newNext){
+        next = newNext;
+    }
+
+    public File getTarget(){
+        return target;
+    }
+
+    private String urlSafe(String input) throws Exception{
+        return new URI(null, input, null).toASCIIString();
+    }
+
+    public void generate() throws Exception{
         String title = target.getName().split("[.]")[0];
         int holeNum = Integer.parseInt(title.split(" ")[1]);
-        System.out.println("{\"" + title + "\", " + holeNum + ", " + Arrays.toString(images) + "}");
+        PrintWriter pw = new PrintWriter(target, StandardCharsets.UTF_8);
+        pw.print("<!DOCTYPE html>\n<html>\n<head>\n<title>");
+        pw.print(title);
+        pw.print(" - ");
+        pw.print(target.getParentFile().getName());
+        pw.print("</title>\n<link rel=\"stylesheet\" href=\"../../gwyf.css\">\n");
+        if(images.length > 1) pw.print("<script src=\"../../midline.js\"></script>\n");
+        pw.print("</head>\n<body style=\"background-image: url('");
+        pw.print(images[0].getName());
+        pw.print("')\">\n");
+        pw.print(tab + "<div id=\"blurPage\" class=\"fullSize\"></div>\n");
+        if(images.length > 1){
+            //Right now the following procedure is hardcoded to two images, but the boolean above isn't.
+            //There is a remote possibility of supporting more than two images in the far future.
+            //As of yet, that hasn't seemed particularaly necessary.
+            pw.print(tab + "<img src=\"");
+            pw.print(urlSafe(images[0].getName()));
+            pw.print("\" id=\"leftHalfImage\">\n");
+            pw.print(tab + "<img src=\"");
+            pw.print(urlSafe(images[0].getName()));
+            pw.print("\" id=\"leftHalfPower\">\n");
+            pw.print(tab + "<img src=\"");
+            pw.print(urlSafe(images[1].getName()));
+            pw.print("\" id=\"rightHalfImage\">\n");
+            pw.print(tab + "<img src=\"");
+            pw.print(urlSafe(images[1].getName()));
+            pw.print("\" id=\"rightHalfPower\">\n");
+            pw.print(tab + "<div id=\"midLine\"></div>\n");
+        } else {
+            pw.print(tab + "<img src=\"");
+            pw.print(urlSafe(images[0].getName()));
+            pw.print("\" id=\"fullWidthImage\">\n");
+        }
+        pw.print(tab + "<a href=\"../../index.html\">\n");
+        pw.print(tab + tab + "<img src=\"../../House.svg\" id=\"homeIcon\">\n");
+        pw.print(tab + "</a>\n");
+        if(previous != null){
+            pw.print(tab + "<a href=\"");
+            pw.print(urlSafe(previous.getName()));
+            pw.print("\">\n");
+            pw.print(tab + tab + "<img src=\"../../Left.svg\" id=\"leftIcon\">\n");
+            pw.print(tab + "</a>\n");
+        }
+        if(next != null){
+            pw.print(tab + "<a href=\"");
+            pw.print(urlSafe(next.getName()));
+            pw.print("\">\n");
+            pw.print(tab + tab + "<img src=\"../../Right.svg\" id=\"rightIcon\">\n");
+            pw.print(tab + "</a>\n");
+        }
+        pw.print(tab + "<div id=\"infoText\">");
+        pw.print(target.getParentFile().getName());
+        pw.print("<br>#");
+        pw.print(holeNum);
+        pw.print("</div>\n</body>\n</html>");
+        pw.close();
     }
 }
