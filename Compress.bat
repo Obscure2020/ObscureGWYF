d660d4e7d85cfbcff7fe7c70bb26cdcc8ad1bd92
Joey Sodergren <jas.elgoog@gmail.com>
2023-07-05 23:31:50 -0400

    Oasis

--- Compress.bat
+++ Compress.bat
@@ -1,13 +1,18 @@
 @echo off
 for /R "%~dp0Official" %%f in (*.png) do (
     pushd "%%~dpf"
-    cwebp -progress -mt -noalpha -q 90 -m 6 -af "%%~nxf" -o "%%~nf.webp"
-    echo.
+    if not exist "%%~nf.webp" (
+        cwebp -progress -mt -noalpha -q 90 -m 6 -af "%%~nxf" -o "%%~nf.webp"
+        echo.
+    )
     popd
 )
 for /R "%~dp0Workshop" %%f in (*.png) do (
     pushd "%%~dpf"
-    cwebp -progress -mt -noalpha -q 90 -m 6 -af "%%~nxf" -o "%%~nf.webp"
+    if not exist "%%~nf.webp" (
+        cwebp -progress -mt -noalpha -q 90 -m 6 -af "%%~nxf" -o "%%~nf.webp"
+        echo.
+    )
     echo.
     popd
 )
