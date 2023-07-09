551c8262c84f86b33badbf25be86b8248e4b1731
Joey Sodergren <jas.elgoog@gmail.com>
2023-07-09 01:59:45 -0400

    Eliminated midline loading flash

--- midline.js
+++ midline.js
@@ -2,5 +2,12 @@ function resizeMidLine(){
     let localHeight = getComputedStyle(document.getElementById("leftHalfImage")).height;
     document.getElementById("midLine").style.height = localHeight;
 }
-window.addEventListener('load', resizeMidLine);
+
 window.addEventListener('resize', resizeMidLine);
+
+function setupMidLine(){
+    document.getElementById("midLine").style.backgroundColor = "#fff";
+    resizeMidLine();
+}
+
+window.addEventListener('load', setupMidLine);
